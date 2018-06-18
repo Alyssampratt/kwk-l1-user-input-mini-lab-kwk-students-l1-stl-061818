81b4163b9c82a5486aadafabc634076de956dbfc
Alyssampratt <magoopratt@gmail.com>
2018-06-18 20:07:01 +0000

    automatically backed up by learn

--- variables.rb
+++ variables.rb
@@ -1,4 +1,4 @@
-name = "alyssa"
-puts "my name is #{name}"
-age = "14 years"
-puts "my name is #{name} and i am #{age}"
+# name = "alyssa"
+# puts "my name is #{name}"
+# age = "14 years"
+# puts "my name is #{name} and i am #{age}"
